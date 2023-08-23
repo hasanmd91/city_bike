03720c155ca9093cd0d04302a819ba096bda6641
hasanmd91 <hasanmd1991@hotmail.com>
2023-08-23 22:24:37 +0300

    delete build

--- backend/build/utils/config/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const port = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
-exports.default = { port, MONGODB_URI };

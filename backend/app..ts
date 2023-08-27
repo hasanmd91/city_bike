4f3d9346bab07ca26fcad98b9b88f81f84970bb7
hasanmd91 <hasanmd1991@hotmail.com>
2023-08-28 01:50:37 +0300

    update morgan logger

--- backend/app..ts
+++ backend/app..ts
@@ -2,19 +2,15 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import 'express-async-errors';
+import * as bodyParser from 'body-parser';
 import { errorHandeler, unknownEndpoint } from './middleware';
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static('build'));
 app.use(express.json());
-
-morgan.token('body', (req) => JSON.stringify(req.body));
-app.use(
-  morgan(
-    ':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'
-  )
-);
+app.use(morgan('combined'));
 
 app.use(unknownEndpoint);
 app.use(errorHandeler);
